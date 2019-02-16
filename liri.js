94c943fb9951341276c853c8b2458f5f5f4750b9
ShelbyM <shelby@shelbymarks.com>
2019-02-15 22:00:04 -0600

    2/15 V2.0

--- liri.js
+++ liri.js
@@ -1,36 +1,27 @@
 
+//I'm only here because the directions told me too
 require("dotenv").config();
 
+///Makin' some Variable for future stuff
 var keys = require("./keys.js");
 var moment = require("moment");
 var Spotify = require("node-spotify-api");
 var axios = require("axios");
-/////
+var fs = require("fs");
 
 
+///THE MEAT - where the sausage is made
 
-// OMDB ALIEN Representative Example
+// using switches to direct specific actions from node, originally based on 10.1 calculator logic
 
-
-///
-
-///THE MEAT - where the sausage i made
-// Get the spotify stuff
-var spotify = new Spotify(keys.spotify);
-
-// (referenced in class activity 15-BankJS):
-
-// store user input (or action requested) in variable
 var action = process.argv[2];
-
-// use switch case to direct which function will run with command line code
 switch (action) {
     case "concert-this":
         concert();
         break;
 
     case "spotify-this-song":
-        spotify();
+        spotifycall();
         break;
 
     case "movie-this":
@@ -45,8 +36,8 @@ switch (action) {
         console.log("Please check your command!");
 }
 
-// =====================FUNCTIONS=============================
-// (adapted from in class activities)
+// Making some functions to make the axios calls / spotify queries / absurd read function for the Fs file, what an absurd ask
+//
 
 function concert() {
     var performer = process.argv[3];
@@ -55,34 +46,36 @@ function concert() {
         .then(function (response) {
             console.log(response.data);
 
-        }
-    }
-function spotify() {
-                var performer = process.argv[4];
+        })
+}
 
-                axios.get()
-                    .then(function (response) {
-                        console.log(response.data);
-                    }
-                }
+function spotifycall() {
+    var performer = process.argv[3];
+
+    var spotify = new Spotify(keys.spotify);
+
+    spotify.search({ type: 'track', query: performer }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        console.log(data.tracks.items[0].album);
+        //   console.log("album:" + data.tracks.items[0].album.name)
+    });
+}
 
 function movie() {
-                            var show = process.argv[5];
+    var show = process.argv[3];
 
-                            axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy")
-                                .then(function (response) {
-                                    console.log(response.data);
-                                }
-    }
-                // loop through response.data.legnth to display:
-                // 1. name of venue
-                // console.log("Venue " + response.data.venue.nameorsomething);
-                // 2. venue location
-                // console.log("Venue Location: " + response.data.venue.locationorsomething);
-                // 3. date of the event 
-                // (use moment to format at MM/DD/YYYY)
-                // store response.data.dateorwhater in variable
-                // var date = moment(response.data.dateorwhatever).format("MM/DD/YYYY");
-                // console.log("Date: " + date);
-            })
+    axios.get("http://www.omdbapi.com/?t=" + show + "&y=&plot=short&apikey=trilogy")
+        .then(function (response) {
+            console.log(response.data);
+        })
+}
+
+
+function doWhat() {
+    fs.readFile("random.txt", "utf-8", function (error, data) {
+        console.log(data);
+
+    })
 }
